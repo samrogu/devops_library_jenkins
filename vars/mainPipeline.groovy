e387ed8af986fc17185a48ada6a87a485467c348
Samuel Rodriguez <grd.rodrigez13@gmail.com>
2021-07-03 22:29:00 -0600

    Update mainPipeline.groovy

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -1,14 +1,18 @@
 def call(int buildNumber) {
   if (buildNumber % 2 == 0) {
     pipeline {
-      agent any
+      agent none
       stages {
+	agent any;
         stage('Even Stage') {
           steps {
             echo "The build number is even"
           }
         }
         stage('Maven Event Stage') {
+	agent {
+	      label "maven-exec"
+	  }
           steps{
 				container('maven') {
 					sh 'mvn --version'
@@ -27,6 +31,9 @@ def call(int buildNumber) {
           }
         }
         stage('Maven Old Stage') {
+		agent {
+	      label "maven-exec"
+	  }
 			steps{
 				container('maven') {
 					sh 'mvn -B'
