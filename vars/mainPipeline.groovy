b15af6793382599b95d9eb037791e8e1aea2c8dc
Samuel Rodriguez <grd.rodrigez13@gmail.com>
2021-07-03 22:48:39 -0600

    Update mainPipeline.groovy

--- vars/mainPipeline.groovy
+++ vars/mainPipeline.groovy
@@ -5,7 +5,7 @@ def call(int buildNumber) {
       stages {
         stage('Even Stage') {
 	  agent {
-	      label "principal"
+	      label "Principal"
 	  }
           steps {
             echo "The build number is even"
@@ -29,7 +29,7 @@ def call(int buildNumber) {
       stages {
         stage('Odd Stage') {
 	agent {
-	      label "principal"
+	      label "Principal"
 	  }
           steps {
             echo "The build number is odd"
